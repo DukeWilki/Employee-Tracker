5230f3468a4a1498e347b1d588c4f13f14e6ea0b
DukeWilki <65857753+DukeWilki@users.noreply.github.com>
2020-11-08 13:44:34 +1100

    all employees. department filter hard-coded on dev

--- tracker.js
+++ tracker.js
@@ -11,8 +11,8 @@ var connection = mysql.createConnection({
   user: "root",
 
   // Your password
-  password: ".",
-  database: "top_songsDB",
+  password: "Queenw00d.",
+  database: "employee_trackerDB",
 });
 
 connection.connect(function (err) {
@@ -41,11 +41,11 @@ function runSearch() {
     .then(function (answer) {
       switch (answer.action) {
         case "View all employees":
-          connection.end();
+            viewAll();
           break;
 
         case "View employees by department":
-          connection.end();
+            v=byDepartment();
           break;
 
         case "View employees by manager":
@@ -70,3 +70,81 @@ function runSearch() {
       }
     });
 }
+
+    function viewAll() {
+        // inquirer
+        //   .prompt([
+        //     {
+        //       name: "start",
+        //       type: "input",
+        //       message: "Enter starting position: ",
+        //       validate: function(value) {
+        //         if (isNaN(value) === false) {
+        //           return true;
+        //         }
+        //         return false;
+        //       }
+        //     },
+        //     {
+        //       name: "end",
+        //       type: "input",
+        //       message: "Enter ending position: ",
+        //       validate: function(value) {
+        //         if (isNaN(value) === false) {
+        //           return true;
+        //         }
+        //         return false;
+        //       }
+        //     }
+        //   ])
+        //   .then(function(answer) {
+            var query = "SELECT first_name, last_name, role_id, roles.title AS 'role', departments.name AS 'department' FROM employees INNER JOIN roles ON role_id = roles.id INNER JOIN departments ON roles.department_id = departments.id"
+            connection.query(query, function(err, res) {
+              for (var i = 0; i < res.length; i++) {
+                console.log(
+                  "Staff Member: " +
+                    res[i].first_name +
+                    " " +
+                    res[i].last_name +
+                    " || Team: " +
+                    res[i].department +
+                    " || Role: " +
+                    res[i].role
+                );
+              }
+              runSearch();
+            });
+        //   });
+      }
+
+
+      function byDepartment() {
+        inquirer
+        .prompt({
+            name: "action",
+            type: "list",
+            message: "Which department?",
+            choices: [
+              "Management",
+              "Human Resources",
+              "Development",
+              "Sales",
+            ],
+        })
+          .then(function(answer) {
+            var query = "SELECT first_name, last_name, role_id, roles.title AS 'role', salary FROM employees INNER JOIN roles ON role_id = roles.id INNER JOIN departments ON roles.department_id = departments.id WHERE departments.name = 'Development'"
+            connection.query(query, function(err, res) {
+              for (var i = 0; i < res.length; i++) {
+                console.log(
+                  "Staff Member: " +
+                    res[i].first_name +
+                    " " +
+                    res[i].last_name +
+                    " || Role: " +
+                    res[i].role
+                );
+              }
+              runSearch();
+            });
+          });
+        }
