c66cc3e3a78190eb4f5bfc50829474b553ad173a
DukeWilki <65857753+DukeWilki@users.noreply.github.com>
2020-11-08 22:12:52 +1100

    all emplyees functional, department functional, remove started, manager started

--- tracker.js
+++ tracker.js
@@ -45,19 +45,19 @@ function runSearch() {
           break;
 
         case "View employees by department":
-            v=byDepartment();
+            byDepartment();
           break;
 
         case "View employees by manager":
-          connection.end();
+            byManager();
           break;
 
         case "Add employee":
-          connection.end();
+            addEmployee();
           break;
 
         case "Remove employee":
-          connection.end();
+            removeEmployee();
           break;
 
         case "Update employee role":
@@ -71,33 +71,8 @@ function runSearch() {
     });
 }
 
+    // VEIW ALL EMPLOYEES
     function viewAll() {
-        // inquirer
-        //   .prompt([
-        //     {
-        //       name: "start",
-        //       type: "input",
-        //       message: "Enter starting position: ",
-        //       validate: function(value) {
-        //         if (isNaN(value) === false) {
-        //           return true;
-        //         }
-        //         return false;
-        //       }
-        //     },
-        //     {
-        //       name: "end",
-        //       type: "input",
-        //       message: "Enter ending position: ",
-        //       validate: function(value) {
-        //         if (isNaN(value) === false) {
-        //           return true;
-        //         }
-        //         return false;
-        //       }
-        //     }
-        //   ])
-        //   .then(function(answer) {
             var query = "SELECT first_name, last_name, role_id, roles.title AS 'role', departments.name AS 'department' FROM employees INNER JOIN roles ON role_id = roles.id INNER JOIN departments ON roles.department_id = departments.id"
             connection.query(query, function(err, res) {
               for (var i = 0; i < res.length; i++) {
@@ -114,26 +89,30 @@ function runSearch() {
               }
               runSearch();
             });
-        //   });
       }
 
-
+    // VIEW ALL EMPLOYEES IN A DEPARTMENT
       function byDepartment() {
+            var query = "SELECT * FROM departments"
+            connection.query(query, function(err, res) {
+                console.log(res);
+                const deptOptions = res.map(function(department) {
+                    return {
+                    name: department.name,
+                    value: department.id
+                    }
+                })
         inquirer
         .prompt({
             name: "action",
             type: "list",
             message: "Which department?",
-            choices: [
-              "Management",
-              "Human Resources",
-              "Development",
-              "Sales",
-            ],
-        })
-          .then(function(answer) {
-            var query = "SELECT first_name, last_name, role_id, roles.title AS 'role', salary FROM employees INNER JOIN roles ON role_id = roles.id INNER JOIN departments ON roles.department_id = departments.id WHERE departments.name = 'Development'"
-            connection.query(query, function(err, res) {
+            choices: deptOptions
+            })
+            .then(function(answer) {
+                console.log(answer);
+            var query = "SELECT first_name, last_name, role_id, roles.title AS 'role', salary FROM employees INNER JOIN roles ON role_id = roles.id INNER JOIN departments ON roles.department_id = departments.id WHERE departments.id = ?"
+            connection.query(query, [answer.action], function(err, res) {
               for (var i = 0; i < res.length; i++) {
                 console.log(
                   "Staff Member: " +
@@ -141,10 +120,176 @@ function runSearch() {
                     " " +
                     res[i].last_name +
                     " || Role: " +
-                    res[i].role
+                    res[i].role +
+                    " || Salary: $" +
+                    res[i].salary
                 );
               }
               runSearch();
             });
           });
+        // }
+        })
+    }
+
+
+      // VIEW ALL EMPLOYEES BY MANAGER
+      function byManager() {
+        var query = "SELECT id, first_name, last_name FROM employees WHERE ismanager = true"
+        connection.query(query, function(err, res) {
+            console.log(res);
+            const mgrOptions = res.map(function(employee) {
+                return {
+                // fname: employee.first_name,
+                name: employee.first_name + " " + employee.last_name,
+                value: {id: employee.id, name: employee.first_name + " " + employee.last_name}
+                }
+            })
+    // inquirer
+    // .prompt({
+    //     name: "manager",
+    //     type: "list",
+    //     message: "Which department?",
+    //     choices: deptOptions
+    //     })
+    //     .then(function(answer) {
+    //         console.log(answer);
+    //     var query = "SELECT first_name, last_name, role_id, roles.title AS 'role', salary FROM employees INNER JOIN roles ON role_id = roles.id INNER JOIN departments ON roles.department_id = departments.id WHERE departments.id = ?"
+    //     connection.query(query, [answer.action], function(err, res) {
+    //       for (var i = 0; i < res.length; i++) {
+    //         console.log(
+    //           "Staff Member: " +
+    //             res[i].first_name +
+    //             " " +
+    //             res[i].last_name +
+    //             " || Role: " +
+    //             res[i].role +
+    //             " || Salary: $" +
+    //             res[i].salary
+    //         );
+    //       }
+    //       runSearch();
+    //     });
+    //   });
+    // }
+    })
+}
+
+        
+        // function addEmployee() {
+        //     inquirer
+        //     .prompt([
+        //         {
+        //           type: "input",
+        //           name: "first_name",
+        //           message: "Enter their first name?"
+        //         },
+        //         {
+        //           type: "input",
+        //           name: "last_name",
+        //           message: "Enter their last name?"
+        //         },
+        //         {
+        //           type: "input",
+        //           name: "role",
+        //           message: "What is their role?"
+        //         },
+        //         {
+        //           type: "input",
+        //           name: "manager",
+        //           message: "Who is their manager?"
+        //         }
+        //       ])
+        //       .then(function(answer) {
+        //         var query = "INSERT INTO employees (first_name, last_name, role_id, manager_id) VALUES ('Carol','Danvers', 3, 1)"
+        //         connection.query(query, function(err, res) {
+        //           for (var i = 0; i < res.length; i++) {
+        //             console.log(
+        //               "Staff Member: " +
+        //                 res[i].first_name +
+        //                 " " +
+        //                 res[i].last_name +
+        //                 " || Role: " +
+        //                 res[i].role
+        //             );
+        //           }
+        //           runSearch();
+        //         });
+        //       });
+        //     }
+
+
+
+
+        function removeEmployee() {
+            var query = "SELECT id, first_name, last_name FROM employees"
+            connection.query(query, function(err, res) {
+                console.log(res);
+                const empOptions = res.map(function(employee) {
+                    return {
+                    // fname: employee.first_name,
+                    name: employee.first_name + " " + employee.last_name,
+                    value: {id: employee.id, name: employee.first_name + " " + employee.last_name}
+                    }
+                })
+
+            inquirer
+            .prompt({
+                name: "remove",
+                type: "list",
+                message: "Which employee?",
+                choices: empOptions
+                })
+
+            
+            .then(function(answer) {
+                console.log(answer);
+            var query = "DELETE FROM employees WHERE id = ?"
+            console.log(query);
+            connection.query(query, [answer.remove.id], function(err, res) {
+                console.log(err)
+            //   for (var i = 0; i < res.length; i++) {
+                console.log(
+                    answer.remove.name +
+                    " is no longer with us."
+                );
+            //   }
+              runSearch();
+            });
+          });
+        })
+        // }
+        // })
+            
+        
+    //   function removeEmployee() {
+    //     inquirer
+    //     .prompt([
+    //         name: "remove",
+    //         type: "list",
+    //         message: "Who would you like to remove?",
+    //         choices: [
+    //           "Peter Quill",
+    //           "Clint Barton",
+    //           "Bucky Barnes"
+    //         ],
+    //       ])
+    //       .then(function(answer) {
+    //         var query = "DELETE FROM employees WHERE first_name='Clint AND first_name='Barton'"
+    //         connection.query(query, function(err, res) {
+    //           for (var i = 0; i < res.length; i++) {
+    //             console.log(
+    //               "Staff Member: " +
+    //                 res[i].first_name +
+    //                 " " +
+    //                 res[i].last_name +
+    //                 " || Role: " +
+    //                 res[i].role
+    //             );
+    //           }
+    //           runSearch();
+    //         });
+    //       });
         }
+
+
